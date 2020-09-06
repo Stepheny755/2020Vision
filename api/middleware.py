a3cbc2aad09256f48bbd798bb2e0ec9e3e0df489
Abhay Gopinathan <abhay.gopinathan@gmail.com>
2020-09-06 15:16:19 +0530

    Changed upload_file to accept url instead of path to wave file

--- api/middleware.py
+++ api/middleware.py
@@ -3,6 +3,7 @@ from google.cloud import storage
 #Need to use actual inputs instead of placeholders
 def upload_file(sourceFile, index):
     import os
+    import urllib.request
 
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./smiling-audio-288604-bfa5f98402cd.json"
 
@@ -10,9 +11,13 @@ def upload_file(sourceFile, index):
     # bucket_name = "your-bucket-name"
     # source_file_name = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
+    wavFile = "./wav/" + str(index) + ".wav"
+    sourceFileURL = sourceFile + ".wav"
+
+    urllib.request.urlretrieve (sourceFileURL, wavFile)
 
     bucket_name = "letter-clips"
-    source = convert(sourceFile, index)
+    source = convert(wavFile, index)
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
