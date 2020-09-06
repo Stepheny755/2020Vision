b87b986d6506c163a53e63b148adc9f52b15d043
Isha Sharma <isha.sha14@gmail.com>
2020-09-06 01:33:09 -0700

    Comment out some code

--- static/js/script.js
+++ static/js/script.js
@@ -159,25 +159,25 @@ function createDownloadLink(blob) {
 }
 
 
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
+// function startTimer(duration, display) {
+//   var timer = duration, minutes, seconds;
+//   setInterval(function () {
+//       minutes = parseInt(timer / 60, 10);
+//       seconds = parseInt(timer % 60, 10);
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+//       minutes = minutes < 10 ? "0" + minutes : minutes;
+//       seconds = seconds < 10 ? "0" + seconds : seconds;
 
-      display.textContent = minutes + ":" + seconds;
+//       display.textContent = minutes + ":" + seconds;
 
-      if (--timer < 0) {
-          timer = duration;
-      }
-  }, 1000);
-}
+//       if (--timer < 0) {
+//           timer = duration;
+//       }
+//   }, 1000);
+// }
 
-window.onload = function () {
-  var fiveSeconds = 5,
-      display = document.querySelector('#time');
-  startTimer(fiveSeconds, display);
-};
+// window.onload = function () {
+//   var fiveSeconds = 5,
+//       display = document.querySelector('#time');
+//   startTimer(fiveSeconds, display);
+// };
